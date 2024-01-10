a289a134794985a40815b53b7ad4b254aeddaf67
GabrielaArizaA <132161427+GabrielaArizaA@users.noreply.github.com>
2024-01-10 11:32:20 +0100

    Create stringtie.sh

---
+++ stringtie.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=stringtie
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=4G
+#SBATCH --time=10:00:00
+#SBATCH --partition=basic
+#SBATCH --output=/scratch/course/2023w300106/arizaaranguren/day3/stringtie.log
+#SBATCH --error=/scratch/course/2023w300106/arizaaranguren/error/stringtie.err
+
+#loading modules
+module load stringtie
+module list
+
+#Running stringtie
+stringtie /scratch/course/2023w300106/BAMS/*.bam -l NemVecv1 -o nemAnnotation.gtf -p 6 -m 230 -f 0.02 -g 60
