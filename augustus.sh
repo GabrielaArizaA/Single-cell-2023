0843f21c6cbe4f1a8603b4f9b2e3b5159c37f277
GabrielaArizaA <132161427+GabrielaArizaA@users.noreply.github.com>
2024-01-10 15:15:49 +0100

    Create augustus.sh

---
+++ augustus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=Augustus
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --time=24:00:00
+#SBATCH --partition=basic
+#SBATCH --output=/scratch/course/2023w300106/arizaaranguren/day3/augustus.log
+#SBATCH --error=/scratch/course/2023w300106/arizaaranguren/error/augustus.err
+
+#loading modules
+module load conda
+module list
+conda activate augustus-3.5.0
+
+#running augustus
+augustus --strand=both --genemodel=complete --gff3=on --UTR=on --outfile nemVec_augustus.gff \
+--species=nematostella_vectensis /scratch/course/2023w300106/arizaaranguren/day1/ex4/ncbi_dataset/data/GCF_932526225.1/chr2.fasta
